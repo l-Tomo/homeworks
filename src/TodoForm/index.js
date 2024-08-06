27d8be28dd42d8a0905ccdbf00b0e66de73f1a3d
l-Tomo <144460796+l-Tomo@users.noreply.github.com>
2024-08-06 15:44:47 -0500

    lol

--- src/TodoForm/index.js
+++ src/TodoForm/index.js
@@ -27,7 +27,7 @@ function TodoForm() {
     <form onSubmit={onSubmit}>
       <label>Esribe tu nuevo TODO</label>
       <textarea
-        placeholder="Cortar cebolla para el almuerzo"
+        placeholder="Usar RadAway"
         value={newTodoValue}
         onChange={onChange}
       />
